7bf2b2a66c3b156f2fb350b2feeb65bd8b08e5a8
kien <nguyentrungkienhp202@gmail.com>
2026-06-11 08:37:56 +0700

    GETPOST

--- apps/backend/AuthService/urls.py
+++ apps/backend/AuthService/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import homePageView
+from .views import role_api
 
 urlpatterns = [
-    path('home/', homePageView, name='home')
+    path('home/', homePageView, name='home'),
+    path('roles/', role_api, name='role-api')
+    
 ]
--- apps/backend/AuthService/views.py
+++ apps/backend/AuthService/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RoleSerializer
+from .models import Role
+
+@api_view(['GET', 'POST'])
+def role_api(request):
+    if request.method == 'GET':
+        roles = Role.objects.all()
+        serializer = RoleSerializer(roles, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = RoleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+        
+
+
 
 def homePageView(request):
     return HttpResponse('Hello, World')
