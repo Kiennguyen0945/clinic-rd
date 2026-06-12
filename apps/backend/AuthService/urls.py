e4011a0811ee7f996492d92445faeab807536001
kien <nguyentrungkienhp202@gmail.com>
2026-06-12 07:29:30 +0700

    GETPOST1

--- apps/backend/AuthService/urls.py
+++ apps/backend/AuthService/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import homePageView
-from .views import role_api
+from .views import homePageView, role_api, user_api
 
 urlpatterns = [
     path('home/', homePageView, name='home'),
-    path('roles/', role_api, name='role-api')
-    
+    path('roles/', role_api, name='role_api'),
+    path('users/', user_api, name='user-api')
 ]
--- apps/backend/AuthService/views.py
+++ apps/backend/AuthService/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import RoleSerializer
-from .models import Role
+from .models import User, Role
+from .serializers import UserSerializer, RoleSerializer
+
+
 
 @api_view(['GET', 'POST'])
 def role_api(request):
@@ -11,14 +13,27 @@ def role_api(request):
         roles = Role.objects.all()
         serializer = RoleSerializer(roles, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
+    if request.method  == 'POST':
         serializer = RoleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-        
 
+@api_view(['GET', 'POST'])
+def user_api(request):
+    if request.method == 'GET': 
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer=UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+        
 
 
 def homePageView(request):
